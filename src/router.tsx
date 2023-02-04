246c7a4487d69d1b7c2e4c5285de6b4b542be003
Arsyad Ramadhan <blacklistdark1987@gmail.com>
2023-02-04 23:39:11 +0700

    Add router

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from '@react-navigation/native-stack';
 import Transactions from './screens/Transactions';
+import TransactionDetail from './screens/TransactionDetail';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  TransactionDetail: {
+    title: string;
+  };
+  Transactions: undefined;
+};
+
+export type NavigationProps = NativeStackNavigationProp<RootStackParamList>;
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const Router = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Transactions"
-        screenOptions={{
-          headerShown: false,
-        }}>
+        screenOptions={{headerBackTitle: ''}}>
         <Stack.Screen name="Transactions" component={Transactions} />
+        <Stack.Screen
+          name="TransactionDetail"
+          component={TransactionDetail}
+          options={({route}) => ({title: route.params.title})}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
